9589a3119348a514011e76352bf9be0c9109f67f
ldm0408 <ldm0408@gmail.com>
2018-05-27 16:45:15 +0900

    create Like models in Image app & modified Comment, Image models in Image app

--- dongmingram/users/models.py
+++ dongmingram/users/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-
+    ''' User Model '''
     GENDER_CHOICES = (
         ('male','Male'),
         ('female', 'Female'),
