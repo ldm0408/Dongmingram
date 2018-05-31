3b5d2644dde177828e8e2c6d65bd1fa51103a576
ldm0408 <ldm0408@gmail.com>
2018-05-31 19:43:19 +0900

    created a serializer and urls, views in Images folder and test a urls

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from django.conf.urls import url
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -20,6 +21,7 @@ urlpatterns = [
         include("dongmingram.users.urls", namespace="users"),
     ),
     path("accounts/", include("allauth.urls")),
+    url(r'^images/', include('dongmingram.images.urls', namespace= 'images')),
     # Your stuff: custom urls includes go here
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
