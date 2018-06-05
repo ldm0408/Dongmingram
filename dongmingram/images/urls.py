afd938795e13400543dc603c2d1c5605da676b73
ldm0408 <ldm0408@gmail.com>
2018-06-05 17:20:19 +0900

    modigied a /images/urls.py--> using path

--- dongmingram/images/urls.py
+++ dongmingram/images/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
 app_name = "images"
 urlpatterns = [
-    url(
-        regex = '^all/$',
+    path(
+        "all/",
         view = views.ListAllImages.as_view(),
         name = 'all_images'
     )
