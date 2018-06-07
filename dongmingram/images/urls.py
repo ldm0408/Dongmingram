8d95f0ff5afd513c89a9ac5e8095729f67f71a24
ldm0408 <ldm0408@gmail.com>
2018-06-07 17:49:00 +0900

    comment set and like set

--- dongmingram/images/urls.py
+++ dongmingram/images/urls.py
@@ -8,5 +8,15 @@ urlpatterns = [
         "all/",
         view = views.ListAllImages.as_view(),
         name = 'all_images'
+    ),
+    path(
+        "comments/",
+        view = views.ListAllComments.as_view(),
+        name = 'all_comments'
+    ),
+    path(
+        "likes/",
+        view = views.ListAllLikes.as_view(),
+        name = 'all_likes'
     )
 ]
