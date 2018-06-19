3ded1f58a191f6c51942421e801306df5e3e22ef
ldm0408 <ldm0408@gmail.com>
2018-06-19 18:56:29 +0900

    create a FeedUserSerializer and modified CommentSerializer, ImageSerializer

--- dongmingram/images/serializers.py
+++ dongmingram/images/serializers.py
@@ -1,12 +1,28 @@
 from rest_framework import serializers
 from . import models
+from dongmingram.users import models as user_models
 
+
+class FeedUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = user_models.User
+        fields = (
+            'username',
+            'profile_image'
+        )
 
 class CommentSerializer(serializers.ModelSerializer):
 
+    creator = FeedUserSerializer()
+    
     class Meta:
         model = models.Comment
-        fields = '__all__'
+        fields = (
+            'id',
+            'creator',
+            'message'
+        )
 
 
 class LikeSerializer(serializers.ModelSerializer):
@@ -16,10 +32,12 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 
+
 class ImageSerializer(serializers.ModelSerializer):
 
     comments = CommentSerializer(many = True)
-    likes = LikeSerializer(many = True)
+    # likes = LikeSerializer(many = True)
+    creator = FeedUserSerializer()
 
     class Meta:
         model = models.Image
@@ -29,5 +47,6 @@ class ImageSerializer(serializers.ModelSerializer):
             'location',
             'caption',
             'comments',
-            'likes'
+            'like_counter',
+            'creator'
         )
