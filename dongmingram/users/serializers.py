11deca7de3c546d5bb905ae9f775c440e88bc4aa
ldm0408 <ldm0408@gmail.com>
2018-07-12 19:42:17 +0900

    add a updating profile function in user profile view

--- dongmingram/users/serializers.py
+++ dongmingram/users/serializers.py
@@ -6,7 +6,9 @@ from dongmingram.images import serializers as image_serializers
 class UserProfileSerializer(serializers.ModelSerializer):
 
     images = image_serializers.CountImageSerializer(many=True)
-
+    post_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    followings_count = serializers.ReadOnlyField()
     class Meta:
         model = models.User
         fields = (
