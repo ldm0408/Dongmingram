bf3580d311306182f42c85cb3653e575a70a07a5
ldm0408 <ldm0408@gmail.com>
2018-08-09 14:38:04 +0900

    connect React and Django

--- config/settings/base.py
+++ config/settings/base.py
@@ -73,6 +73,7 @@ THIRD_PARTY_APPS = [
     'taggit_serializer',
     'rest_auth',
     'rest_auth.registration',
+    'corsheaders', # To accept requests from React
 ]
 LOCAL_APPS = [
     'dongmingram.users.apps.UsersConfig',
@@ -134,6 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -150,6 +152,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     str(APPS_DIR.path('static')),
+    str(ROOT_DIR.path('frontend', 'build', 'static'))
 ]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
@@ -256,3 +259,5 @@ REST_FRAMEWORK = {
 REST_USE_JWT = True
 
 ACCOUNT_LOGOUT_ON_GET = True ## Get 으로 로그아웃을 사용하기 위한 코드
+
+CORS_ORIGIN_ALLOW_ALL = True 
