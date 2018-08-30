e2fa083e9778a2a6901ce51adebd27c80bbac604
ldm0408 <ldm0408@gmail.com>
2018-08-30 15:35:33 +0900

    redux thunk 설치- 리액트앱과 스토어 사이를 연결해준다,리덕스 스토어로 액션을 보낼수있다

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./redux/configureStore";
-import "./index.css";
-import App from "./App";
+import store from "redux/configureStore";
+import "index.css";
+import App from "App";
 
 console.log(store.getState());
 
 ReactDOM.render(
-  <Provider store={store()}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
